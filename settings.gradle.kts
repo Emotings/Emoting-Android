3f2f59f2a444d39eebf191bb1bb5e55410a28ba8
Tmdhoon2 <hks026naver@gmail.com>
2024-02-14 23:57:07 +0900

    chore :: 프로젝트 세팅

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "emoting-android"
+include(":app")
+include(":designsystem")
