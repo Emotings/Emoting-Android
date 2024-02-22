f22dfddb4e735204602d812144e61baa0b613d83
Tmdhoon2 <hks026naver@gmail.com>
2024-02-22 20:11:14 +0900

    feat :: SplashScreen

--- app/src/main/java/com/emoting/android/MainActivity.kt
+++ app/src/main/java/com/emoting/android/MainActivity.kt
@@ -3,12 +3,17 @@ package com.emoting.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import com.emoting.designsystem.ui.theme.EmotingAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            // ProjectGeneratorTheme {}
+            EmotingAndroidTheme {
+                EmotingApp()
+            }
         }
     }
 }
