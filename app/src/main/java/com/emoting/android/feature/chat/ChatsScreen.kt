c4f15ca11615c7cd513678c8819cabcaa49e27eb
Tmdhoon2 <hks026naver@gmail.com>
2024-02-23 11:25:35 +0900

    feat :: ChatContent 추상화

--- app/src/main/java/com/emoting/android/feature/chat/ChatsScreen.kt
+++ app/src/main/java/com/emoting/android/feature/chat/ChatsScreen.kt
@@ -4,16 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
@@ -21,16 +17,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.emoting.android.R
+import com.emoting.android.ui.ChatContent
 import com.emoting.designsystem.ui.button.ButtonColor
 import com.emoting.designsystem.ui.button.EmotingFloatingButton
 import com.emoting.designsystem.ui.theme.EmotingColors
 import com.emoting.designsystem.ui.theme.EmotingTypography
-import com.emoting.designsystem.utils.clickable
 
 private data class Chat(
     val profile: String,
@@ -88,60 +82,13 @@ private fun Chats(
 ) {
     LazyColumn(verticalArrangement = Arrangement.spacedBy(28.dp)) {
         items(chats) {
-            ChatContent(chat = it)
-        }
-    }
-}
-
-@Composable
-private fun ChatContent(
-    chat: Chat,
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(
-                onClick = {},
-                pressDepth = 0.98f,
-            ),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        AsyncImage(
-            modifier = Modifier
-                .size(48.dp)
-                .clip(CircleShape),
-            model = chat.profile,
-            contentDescription = "chat profile",
-        )
-        Column {
-            Text(
-                text = chat.name,
-                style = EmotingTypography.TextMedium,
+            ChatContent(
+                profile = it.profile,
+                name = it.name,
+                boolean = it.hasNewMessage,
+                trueMessage = stringResource(id = R.string.not_read_message),
+                falseMessage = stringResource(id = R.string.read_all_message),
             )
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(4.dp),
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(12.dp)
-                        .clip(CircleShape)
-                        .background(
-                            color = if (chat.hasNewMessage) EmotingColors.Primary500
-                            else EmotingColors.Gray300,
-                        )
-                )
-                Text(
-                    text = stringResource(
-                        id = if (chat.hasNewMessage) R.string.not_read_message
-                        else R.string.read_all_message,
-                    ),
-                    style = EmotingTypography.TextSmall,
-                    color = if (chat.hasNewMessage) EmotingColors.Primary500
-                    else EmotingColors.Gray300,
-                )
-            }
         }
     }
 }
---
+++ app/src/main/java/com/emoting/android/ui/ChatContent.kt
@@ -0,0 +1,75 @@
+package com.emoting.android.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.emoting.designsystem.ui.theme.EmotingColors
+import com.emoting.designsystem.ui.theme.EmotingTypography
+import com.emoting.designsystem.utils.clickable
+
+@Composable
+fun ChatContent(
+    profile: String,
+    name: String,
+    boolean: Boolean,
+    trueMessage: String,
+    falseMessage: String,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(
+                onClick = {},
+                pressDepth = 0.98f,
+            ),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .size(48.dp)
+                .clip(CircleShape),
+            model = profile,
+            contentDescription = "chat profile",
+        )
+        Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
+            Text(
+                text = name,
+                style = EmotingTypography.TextMedium,
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(12.dp)
+                        .clip(CircleShape)
+                        .background(
+                            color = if (boolean) EmotingColors.Primary500
+                            else EmotingColors.Gray300,
+                        )
+                )
+                Text(
+                    text = if (boolean) trueMessage
+                    else falseMessage,
+                    style = EmotingTypography.TextSmall,
+                    color = if (boolean) EmotingColors.Primary500
+                    else EmotingColors.Gray300,
+                )
+            }
+        }
+    }
+}
