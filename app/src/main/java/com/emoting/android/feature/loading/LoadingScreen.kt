619c405ed62c8fcdcdcff32b92f957aa5fe7253a
Tmdhoon2 <hks026naver@gmail.com>
2024-02-23 23:50:04 +0900

    feat :: LoadingScreen 추가

---
+++ app/src/main/java/com/emoting/android/feature/loading/LoadingScreen.kt
@@ -0,0 +1,20 @@
+package com.emoting.android.feature.loading
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun LoadingScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+
+    }
+}
+
--- app/src/main/java/com/emoting/android/navigation/MainNavigation.kt
+++ app/src/main/java/com/emoting/android/navigation/MainNavigation.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.emoting.android.NavigationRoute
+import com.emoting.android.feature.loading.LoadingScreen
 import com.emoting.android.feature.root.RootScreen
 
 internal fun NavGraphBuilder.mainNavigation(
@@ -11,10 +12,13 @@ internal fun NavGraphBuilder.mainNavigation(
 ) {
     navigation(
         route = NavigationRoute.Main.route,
-        startDestination = NavigationRoute.Main.ROOT,
+        startDestination = NavigationRoute.Main.LOADING,
     ) {
         composable(NavigationRoute.Main.ROOT) {
             RootScreen()
+        }
+        composable(NavigationRoute.Main.LOADING){
+            LoadingScreen()
         }
     }
 }
